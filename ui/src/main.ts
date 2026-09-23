3c8d9bda01a1fb0de582b9bda4f39a348e8fcd87
wangchenghai <1372195290@qq.com>
2026-09-23 16:06:25 +0800

    chore: clear macOS branch

--- ui/src/main.ts
+++
@@ -1,7 +0,0 @@
-// DotWallpaper 壁纸工具 - Vue 3 入口
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import "./styles/main.css";
-
-createApp(App).use(createPinia()).mount("#app");
